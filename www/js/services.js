51db19f33017cb9d2e49ece8198c94fd4b770fe3
RalucaGeorgescu <georgescu.raluca93@gmail.com>
2016-03-12 22:35:05 +0000

    Latest

--- www/js/services.js
+++ www/js/services.js
@@ -1,5 +1,31 @@
 angular.module('starter.services', [])
 
+.service('UserService', function() {
+  // For the purpose of this example I will store user data on ionic local storage but you should save it on a database
+  var setUser = function(user_data) {
+    window.localStorage.starter_facebook_user = JSON.stringify(user_data);
+  };
+
+  var getUser = function(){
+    return JSON.parse(window.localStorage.starter_facebook_user || '{}');
+  };
+
+  return {
+    getUser: getUser,
+    setUser: setUser
+  };
+})
+
+.factory('lessons',['$http', function($http){
+  return $http.get('js/data.json')
+    .success(function(data){
+      return data;
+    })
+    .error(function(data){
+      console.log("Cannot get data");
+    });
+}])
+
 .factory('Chats', function() {
   // Might use a resource here that returns a JSON array
 
