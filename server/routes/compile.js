7e7a4ba56e53c9b4b083a66c8243ecff88d5d420
crisbodnar <crisbodnar96@gmail.com>
2016-03-12 17:49:43 +0000

    messages work

--- server/routes/compile.js
+++ server/routes/compile.js
@@ -3,6 +3,7 @@ var router = express.Router();
 var exec = require('child_process').exec;
 var fs = require('fs');
 var path = require('path');
+var twilio = require('twilio');
 
 //Give a file number that is not used to the file
 var findFileNumber = function(){
@@ -10,9 +11,9 @@ var findFileNumber = function(){
     do{
         number = Math.random(0, 999999999); 
     }while(path.existsSync('lola' + number + '.txt'))
-}   
+} 
 
-router.post('/compile', function(req, res){
+router.post('/', function(req, res){
     //Get the sent source code of the program
     var sourceCode = req.body.sourceCode;
 
@@ -38,10 +39,77 @@ router.post('/compile', function(req, res){
             fs.unlink(pythonFileName);
 
             //Send the resut of the compiling back
-            res.json({compileResult: stdout});1
+            res.json({compileResult: stdout});
         }); //exec01
     }); //writeFile1
 }) //router.post1
+
+var sendMessageBack = function(output){
+    // Twilio Credentials
+    var accountSid = 'AC49beb2b0ce8058005d7db2504507a09f';
+    var authToken = '86bafd16b3b70d18e85e051fefddb6f1';
+
+    //require the Twilio module and create a REST client
+    var twilio = require('twilio');
+    var client = new twilio.RestClient(accountSid, authToken);
+};
+
+router.post('/message', function(req, res){
+    //get the message Sid
+    var messageSid = req.body.SmsMessageSid
+    
+    // Twilio Credentials
+    var accountSid = 'AC49beb2b0ce8058005d7db2504507a09f';
+    var authToken = '86bafd16b3b70d18e85e051fefddb6f1';
+
+    //require the Twilio module and create a REST client
+    var twilio = require('twilio');
+    var client = new twilio.RestClient(accountSid, authToken);
+
+    client.messages(messageSid).get(function(err, message) {
+        //Get the sent source code of the program
+        var sourceCode = req.body.sourceCode;
+
+        //Find an unused fileNumber
+        var fileNumber = findFileNumber();
+
+        //Get a random name for the file
+        var lolaFileName = 'lola' + fileNumber + '.txt';
+        var pythonFileName = 'python' + fileNumber + '.py';
+
+        //Put the sourceCode in a file
+        fs.writeFile(lolaFileName, sourceCode, function(err){
+            if(err)
+                console.log(err);
+
+            //File is written here. Now run the python script
+            exec('python interpreter.py' + lolaFileName + ' ' + pythonFileName, function(err, stdout, sterr){
+                if(err)
+                    console.log(err);
+
+                //Delete the temporary lola and python apps
+                fs.unlink(lolaFileName);
+                fs.unlink(pythonFileName);
+
+                client.sms.messages.create({
+                    to: '+447733645724',
+                    from: '+441376350104',
+                    body: stdout;
+                }, function(err, message) {
+                    if(!err)
+                        console.log(message.sid);
+                    else
+                        console.log(err);
+                });
+
+                //Send the resut of the compiling back
+                res.json({compileResult: stdout});
+            }); //exec01
+        }); //writeFile1
+    });
+})
+
+router.post
 
 //return the router
 module.exports = router;
